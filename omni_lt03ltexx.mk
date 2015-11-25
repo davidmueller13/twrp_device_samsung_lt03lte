3797ac1b9fbafdd18daf55f10d7f9fe842f39403
davidmueller13 <david.mueller1013@gmail.com>
2015-11-25 22:52:56 +0100

    initial commit

---
+++ omni_lt03ltexx.mk
@@ -0,0 +1,37 @@
+# Copyright (C) 2010 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for a full Android
+# build for grouper hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps).
+#
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# This is where we'd set a backup provider if we had one
+#$(call inherit-product, device/sample/products/backup_overlay.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+PRODUCT_COPY_FILES += \
+	device/samsung/lt03ltexx/init.recovery.qcom.rc:root/init.recovery.qcom.rc
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := omni_lt03ltexx
+PRODUCT_DEVICE := lt03ltexx
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := lt03ltexx
+
